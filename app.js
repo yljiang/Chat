ea07b3ecd5225ce91d48f392d9dfaeb08e61a729
yljiang <flaming_dragon29@hotmail.com>
2016-05-23 17:48:15 -0400

    Added private messaging features

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var users = [];
+var users = {};
 
 io.on('connection', function(socket){
 var player = socket.id;
@@ -14,44 +14,78 @@ var player = socket.id;
   console.log('a user connected');
   socket.on('new user', function(data, callback){
 
-    if(users.indexOf(data) != -1){
+    if(data in users){
       callback(false);
     } else {
       callback(true);
       socket.nickname = data;
-      users.push(socket.nickname);
-      console.log("new user added: " + socket.nickname);
+      users[socket.nickname] = socket;
+
       io.emit('user connected', socket.nickname);
       updateOnlineUsers();
     }
    
   });
 
-  function updateOnlineUsers(){
-    var html = '';
-    for(var name of users){
-      html += '<li>' + name + '</li>'; 
-    }
-    console.log(html);
-    io.emit('online users', html);
-  }
-
   socket.on('disconnect', function(){
     io.emit('disconnect', socket.nickname);
-    users.splice(users.indexOf(socket.nickname), 1);
+    
+    delete users[socket.nickname];
+
     updateOnlineUsers();
 
   });
 
   //receive emitted message
-  socket.on('send message', function(msg){
+  socket.on('send message', function(msg, callback){
     console.log('message ' + msg);
-    //broadcast event to everyone
-    io.emit('new message', {name: socket.nickname, msg: msg});
+    
+    msg = msg.trim();
+
+    //check if its a whisper
+    if(msg.substr(0,3) == '/w '){
+      msg = msg.substr(3);
+      var idx = msg.indexOf(' ');
+      if(idx == -1){
+
+        callback('Please enter a message.');
+      } else {
+        
+        var to = msg.substr(0, idx);
+        var pm = msg.substr(idx+1);
+
+        if(!(to in users)){
+
+          callback('That user does not exist.');
+
+        }else{
+          //send private message
+          users[to].emit('pm', {name: socket.nickname, msg: pm.trim()});
+          //display on own chat box
+          // users[socket.nickname].emit('pm', {name: socket.nickname, msg: pm.trim()});
+
+          callback(true);
+        }
+
+      }
+    }else{
+      //broadcast event to everyone
+      io.emit('new message', {name: socket.nickname, msg: msg});
+    }
+
+
   });
 
 });
 
+function updateOnlineUsers(){
+  var html = '';
+  for(var name in users){
+    html += '<li>' + name + '</li>'; 
+  }
+  console.log(html);
+  io.emit('online users', html);
+}
 
 
 http.listen(3000, function(){
